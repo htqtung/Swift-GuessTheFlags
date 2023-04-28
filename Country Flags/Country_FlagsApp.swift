108a2a8772f43a233334fe966f3e0f0cd865a18b
Tung Huynh <htqtung@gmail.com>
2023-04-28 18:33:03 +0300

    Initial Commit

---
+++ Country Flags/Country_FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_FlagsApp.swift
+//  Country Flags
+//
+//  Created by Tung Huynh on 28.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct Country_FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
